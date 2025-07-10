3277c26d5b6378d70f9bf4636851dbf2b4da10b0
piyush kumar <24145@iiitu.ac.in>
2025-07-10 10:42:58 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.REACT_APP_API_BASE_URL;
+export const baseUrl = "http://localhost:5000";
 export const stripePublishableKey = "pk_test_51RcPdGGd8yfmOEEK0l9c8eBdLAQCkERmaTRWsk3t7lZAp49AcyYmyXmDld9pKp56eNIQGGJdFaeONkWnOxMHOqSy00c9Ji2AfQ"
